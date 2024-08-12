df061890eb43ba45a43b2e18b8032d36fb182a22
qingfeng178 <17723648@qq.com>
2024-08-12 18:30:33 +0800

    更新 Drops.js

--- Drops.js
+++ Drops.js
@@ -14,8 +14,8 @@ hostname = api.revenuecat.com
 *************************************/
 
 
-const chxm1023 = {};
-const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const qf = {};
+const qf1 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
 const name = "premium";
 const appid = "forever_unlimited_time_discounted_80_int";
@@ -23,17 +23,17 @@ const appid = "forever_unlimited_time_discounted_80_int";
 if (typeof $response == "undefined") {
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
-  chxm1023.headers = $request.headers;
-} else if (chxm1024 && chxm1024.subscriber) {
-  chxm1024.subscriber.subscriptions = chxm1024.subscriber.subscriptions || {};
-  chxm1024.subscriber.entitlements = chxm1024.subscriber.entitlements || {};
+  qf.headers = $request.headers;
+} else if (qf1 && qf1.subscriber) {
+  qf1.subscriber.subscriptions = qf1.subscriber.subscriptions || {};
+  qf1.subscriber.entitlements = qf1.subscriber.entitlements || {};
   const data = {
 	"product_identifier": (appid),
 	"purchase_date": "2022-09-09T09:09:09Z"
 	};
-  chxm1024.subscriber.entitlements[(name)] = (data);
-  chxm1024.subscriber.subscriptions[(appid)] = {  ...data,	"Author": "chxm1023",	"Telegram": "https://t.me/chxm1023",	"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2022-09-09T09:09:09Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
-  chxm1023.body = JSON.stringify(chxm1024);
+  qf1.subscriber.entitlements[(name)] = (data);
+  qf1.subscriber.subscriptions[(appid)] = {  ...data,	"Author": "qf",	"Telegram": "qf",	"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2022-09-09T09:09:09Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
+  qf.body = JSON.stringify(qf1);
 }
 
-$done(chxm1023);
+$done(qf);
