145c2ca63db67e045a90bcc57adf2d80e99e504b
qingfeng178 <17723648@qq.com>
2024-08-13 02:02:40 +0800

    Update boshu.js

--- boshu.js
+++ boshu.js
@@ -3,8 +3,7 @@
 博树
 
 [rewrite_local]
-^https?:\/\/api\.(busuu|boshu)\.(com|cn)\/users\/me url script-response-body 
-
+^https?:\/\/api\.(busuu|boshu)\.(com|cn)\/users\/me url script-response-body https://raw.githubusercontent.com/qingfeng178/qingfeng/main/boshu.js
 [MITM]
 hostname = api.busuu.com, api.boshu.cn
 
