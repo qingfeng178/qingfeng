f1cb1f4debce9358cc86fcfc2b76f27ddb2df95a
qingfeng178 <17723648@qq.com>
2024-08-12 16:54:48 +0800

    Update xykk.js

--- xykk.js
+++ xykk.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/qingfeng178/qingfeng/main/xykk.js
 
 [mitm]
 hostname = buy.itunes.apple.com
