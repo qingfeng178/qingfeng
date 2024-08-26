96117414ed1b549463b29f5251b28b322e7465e5
qingfeng178 <17723648@qq.com>
2024-08-26 17:26:26 +0800

    Add files via upload

---
+++ gigabody.js
@@ -0,0 +1,48 @@
+/******************************
+
+脚本功能：gigabody——解锁订阅
+下载地址：https://is.gd/Zn4QaU9
+
+*******************************
+
+
+[rewrite_local]
+
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/qingfeng178/qingfeng/main/gigabody.js
+
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/qingfeng178/qingfeng/main/gigabody.js
+
+[mitm] 
+
+hostname = api.revenuecat.com
+
+*******************************/
+
+const py996 = {};
+const py997 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+
+const name = "pro";
+const appid = "GigaBodySubscriptionYear_v1";
+
+  
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+
+  py996.headers = $request.headers;
+} else if (py997 && py997.subscriber) {
+  data = {
+    "expires_date": "9999-09-09T09:09:09Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+
+  py997.subscriber.subscriptions[(appid)] = data
+  py997.subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
+  py997.subscriber.entitlements[(name)].product_identifier = (appid);
+  py996.body = JSON.stringify(py997);
+}
+
+$done(py996);
