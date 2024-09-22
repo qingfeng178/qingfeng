72a7e2429c14433eba8c816b9e285c8ebde9ec3f
qingfeng178 <17723648@qq.com>
2024-09-22 16:01:01 +0800

    更新 Reheji2.js

--- Reheji2.js
+++ Reheji2.js
@@ -6,8 +6,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/qingfeng178/qingfeng/main/Reheji.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/qingfeng178/qingfeng/main/Reheji.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/qingfeng178/qingfeng/main/Reheji2.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/qingfeng178/qingfeng/main/Reheji2.js
 
 [mitm]
 hostname = api.revenuecat.com
