0ac6c3e514fa3ce9f620b590a86aa6f15aa5114f
qingfeng178 <17723648@qq.com>
2024-10-18 16:42:23 +0800

    Add files via upload

---
+++ filmicpro.js
@@ -0,0 +1,26 @@
+/*************************************
+
+项目功能：Filmic Pro相机
+
+**************************************
+
+[rewrite_local]
+^https:\/\/filmicpro\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify) url script-response-body https://raw.githubusercontent.com/qingfeng178/qingfeng/main/filmicpro.js
+
+[mitm]
+hostname = filmicpro.oracle.bendingspoonsapps.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023["me"]["active_subscriptions_ids"] = ["com.cinegenix.filmic.pro.1y_t80_1w_bis"];
+chxm1023["me"]["active_bundle_subscriptions"] = [{
+   "expiry" : "2099-09-09T09:09:09+00:00",
+   "product_id" : "com.cinegenix.filmic.pro.1y_t80_1w_bis",
+   "features" : ["unlock"]
+  }];
+chxm1023["settings"]["__identity__"]["expiration"] = "2099-09-09T09:09:09.09+00:00";
+
+$done({body : JSON.stringify(chxm1023)});
