6a60daf180c56993db04eac1660e82146f34f379
qingfeng178 <17723648@qq.com>
2026-03-15 19:44:49 +0800

    更新 ftzww.js

--- ftzww.js
+++ ftzww.js
@@ -8,15 +8,6 @@ hostname = *.cloudfront.net
 *******************************/
 var qf = JSON.parse($response.qf);
 
-qf = {
-  "paywall": 0,
-  "premium": 1,
-  "expire": "4092599349",
-  "standard": 1,
-  "v": 2099,
-  "campaign_code": "",
-  "latest_duration": "yearly",
-  "addon": 0
-};
+qf = {"paywall":0,"premium":1,"standard":1,"addon":0};
 
 $done({ qf: JSON.stringify(qf) });
