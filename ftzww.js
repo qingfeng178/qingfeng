baf2db73de2c0b54fa74fec7acbed36d30440ac3
qingfeng178 <17723648@qq.com>
2026-03-15 19:36:50 +0800

    更新 ftzww.js

---
+++ ftzww.js
@@ -0,0 +1,22 @@
+***********************
+
+[rewrite_local]
+^https:\/\/.*\.cloudfront\.net\/index\.php\/jsapi\/paywall url script-response-body https://raw.githubusercontent.com/qingfeng178/qingfeng/main/ftzww.js
+[mitm] 
+hostname = *.cloudfront.net
+
+*******************************/
+var qf = JSON.parse($response.qf);
+
+qf = {
+  "paywall": 0,
+  "premium": 1,
+  "expire": "4092599349",
+  "standard": 1,
+  "v": 2099,
+  "campaign_code": "",
+  "latest_duration": "yearly",
+  "addon": 0
+};
+
+$done({ qf: JSON.stringify(qf) });
